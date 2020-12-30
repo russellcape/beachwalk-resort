bfaf75b801c57da3dee05e7a236fa7d66fe46cd7
Russell <russellcape98@gmail.com>
2020-12-30 14:17:53 -0500

    Created banner.js and the animations for it

--- src/components/home/Header.js
+++ src/components/home/Header.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from '../globals/Hero';
 import homeImg from "../../images/homeBcg.jpeg";
-import aboutImg from "../../images/aboutBcg.jpeg";
+// import aboutImg from "../../images/aboutBcg.jpeg";
+import Banner from "../globals/Banner"
 
 const Header = () => {
     return (
         <Hero img={homeImg}>
-            <h1>Hello World!</h1>
+            <Banner greeting="Welcome to" title="beachwalk resort" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus congue scelerisque purus, quis tempus magna." />
+            
         </Hero>
     );
 };
--- src/styles.js
+++ src/styles.js
@@ -1,4 +1,4 @@
-
+import { css } from "styled-components";
 
 export const setColor = {
     primaryColor: "#af9a7d",
@@ -29,3 +29,37 @@ export const setBackground = ({
     url(${img}) center/cover fixed no-repeat
     `);
 };
+
+export const setRem = (number = 16) => {
+    return `${number/16}rem`;
+};
+
+export const setLetterSpacing = (number = 2) => {
+    return `letter-spacing:${number}px`;
+};
+
+export const setBorder = ({ 
+    width="2px", 
+    style="solid", 
+    color="black"
+    }={}) => {
+        return `border:${width} ${style} ${color}`;
+};
+
+const sizes = {
+    Large: 1200,
+    desktop: 992,
+    tablet: 768,
+    phone: 576,
+}
+
+// Iterate through the sizes and create a media template
+export const media = Object.keys(sizes).reduce((acc, label) => {
+    acc[label] = (...args) => css`
+        @media (min-width: ${sizes[label] / 16}em) {
+            ${css(...args)}
+        }
+    `;
+
+    return acc;
+}, {});
