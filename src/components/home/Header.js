4a9e159244060e3d668c3e6111289d4e399dab6c
Russell <russellcape98@gmail.com>
2020-12-30 14:57:08 -0500

    Created global button component

--- src/components/home/Header.js
+++ src/components/home/Header.js
@@ -2,13 +2,17 @@ import React from 'react';
 import Hero from '../globals/Hero';
 import homeImg from "../../images/homeBcg.jpeg";
 // import aboutImg from "../../images/aboutBcg.jpeg";
-import Banner from "../globals/Banner"
+import Banner from "../globals/Banner";
+import { PrimaryBtn } from "../globals/Buttons"
 
 const Header = () => {
     return (
         <Hero img={homeImg}>
-            <Banner greeting="Welcome to" title="beachwalk resort" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus congue scelerisque purus, quis tempus magna." />
-            
+            <Banner greeting="Welcome to" title="beachwalk resort" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus congue scelerisque purus, quis tempus magna.">
+                <PrimaryBtn t="1rem">
+                    view details
+                </PrimaryBtn>
+            </Banner>
         </Hero>
     );
 };
